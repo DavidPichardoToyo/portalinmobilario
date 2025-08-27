3fc1e8efdc360019a817cfcacb480cc0fc818726
DavidPichardoToyo <dpichardot31@gmail.com>
2025-08-27 03:04:56 +0000

    avance proyecto

---
+++ backend/portal/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+# Create your models here.
+
+
+
+class Region(models.Model):
+    nro_region = models.CharField(max_length=5)
+    nombre = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return f"{self.nombre} ||| numero de region es: {self.nro_region}" # Valparaiso ||| numero de region es: V
+
+class Comuna(models.Model):
+    nombre = models.CharField(max_length=50)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE, related_name="comunas")
+
+    def __str__(self):
+        return f"{self.nombre} ||| numero de region es: {self.region}" # Valparaiso ||| numero de region es: Valparaiso
+    
+
+# modelo de inmueble
+
+class Inmueble(models.Model):
+    class Tipo_de_inmueble(models.TextChoices):
+        casa = "CASA", _("casa")
+        departamento = "DEPARTAMENTO", _("departamento")
+        parcela = "PARCELA", _("parcela")
+
+
+
+
+
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    m2_construidos = models.FloatField(default=0)
+    m2_totales = models.FloatField(default=0)
+    estacionamientos = models.PositiveSmallIntegerField(default=0)
+    habitaciones = models.PositiveSmallIntegerField(default=0)
+    banos = models.PositiveSmallIntegerField(default=0)
+    direccion = models.CharField(max_length=100)
+    precio_inmueble = models.DecimalField(max_digits=8, decimal_places=2)
+    creado = models.DateTimeField(auto_now_add=True)
+    actualizado = models.DateTimeField(auto_now=True)
+    comuna = models.ForeignKey(Comuna, on_delete=models.PROTECT, related_name="inmuebles")
+    tipo_de_inmueble = models.CharField(max_length=20, choices=Tipo_de_inmueble.choices)
